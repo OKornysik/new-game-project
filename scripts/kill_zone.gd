ae3fcf8522fb526145512c55319f0bbd12aa38ff
OKornysik <oleksandr.kornysik@gmail.com>
2025-02-04 01:34:25 +0200

    Day 3: - Score - Music and sounds

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	print('You died')
+	audio_stream_player.playing = true
 	Engine.time_scale = 0.5
 	body.get_node('CollisionShape2D').queue_free()
 	timer.start()
